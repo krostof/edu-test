fc1b6319889f0fb08aebc200e8b9315aa5f4ab9f
krostof <krostof4@wp.pl>
2025-05-28 17:28:29 +0200

    first commit

---
+++ edutest-web-server/src/test/java/com/edutest/webserver/EdutestWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutest.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutestWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
