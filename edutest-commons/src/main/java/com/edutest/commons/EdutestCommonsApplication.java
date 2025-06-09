11ce67be4fd28696529779445e00a1a298d850d4
krostof <krostof4@wp.pl>
2025-06-09 23:29:16 +0200

    created all modules

---
+++ edutest-commons/src/main/java/com/edutest/commons/EdutestCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.edutest.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutestCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdutestCommonsApplication.class, args);
+    }
+
+}
---
+++ edutest-domain/src/test/java/com/edutest/domain/EdutestDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutest.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutestDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
