e2251d096875a3329b6415cbf5c1306d1c4b371f
krostof <salacink@wp.pl>
2025-08-31 20:22:40 +0200

    Enabled jpa repo and entity scan

--- edutest-web-server/src/main/java/com/edutest/webserver/EdutestWebServerApplication.java
+++ edutest-web-server/src/main/java/com/edutest/webserver/EdutestWebServerApplication.java
@@ -2,10 +2,14 @@ package com.edutest.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.edutest")
+@EnableJpaRepositories(basePackages = "com.edutest.persistance.repository")
+@EntityScan(basePackages = "com.edutest.persistance.entity")
 public class EdutestWebServerApplication {
 
     public static void main(String[] args) {
