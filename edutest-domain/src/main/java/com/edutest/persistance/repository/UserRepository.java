f6cd4e882dbda6c3c7881fd149346c38844debbb
krostof <salacink@wp.pl>
2025-08-31 20:19:56 +0200

    Added repository annotation

--- edutest-domain/src/main/java/com/edutest/persistance/repository/UserRepository.java
+++ edutest-domain/src/main/java/com/edutest/persistance/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.edutest.persistance.repository;
 
 import com.edutest.persistance.entity.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     Optional<UserEntity> findByUsername(String username);
