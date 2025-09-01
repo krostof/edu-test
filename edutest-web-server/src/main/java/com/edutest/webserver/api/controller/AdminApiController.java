eb33377666c9a68ae6662ede6e37871193635af3
krostof <salacink@wp.pl>
2025-09-01 21:56:26 +0200

    implementing AdminApiController

--- edutest-web-server/src/main/java/com/edutest/webserver/api/controller/AdminApiController.java
+++ edutest-web-server/src/main/java/com/edutest/webserver/api/controller/AdminApiController.java
@@ -2,10 +2,11 @@ package com.edutest.webserver.api.controller;
 
 import com.edutest.api.AdminApi;
 import com.edutest.api.model.CreateStudentRequest;
+import com.edutest.api.model.CreateTeacherRequest;
 import com.edutest.api.model.UserProfile;
 import com.edutest.service.userservice.UserManagementService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,13 @@ public class AdminApiController implements AdminApi {
     private final UserManagementService userManagementService;
 
     @Override
-    public ResponseEntity<UserProfile> createStudent(@Valid CreateStudentRequest body) {
+    public ResponseEntity<UserProfile> createStudent(CreateStudentRequest createStudentRequest) {
+        return AdminApi.super.createStudent(createStudentRequest);
+    }
 
-        UserProfile student = userManagementService.createStudent(body);
-        return ResponseEntity.status(201).body(student);
-
+    @Override
+    public ResponseEntity<UserProfile> createTeacher(CreateTeacherRequest createTeacherRequest) {
+        return AdminApi.super.createTeacher(createTeacherRequest);
     }
 
 }
