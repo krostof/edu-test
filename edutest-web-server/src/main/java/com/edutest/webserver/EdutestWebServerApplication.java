c3d3cbd302d0d6289dbfc41afc58088fcc02e5c7
krostof <salacink@wp.pl>
2025-09-02 22:43:31 +0200

    Added EnableJpaAuditing annotation

--- edutest-web-server/src/main/java/com/edutest/webserver/EdutestWebServerApplication.java
+++ edutest-web-server/src/main/java/com/edutest/webserver/EdutestWebServerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.edutest")
 @EnableJpaRepositories(basePackages = "com.edutest.persistance.repository")
 @EntityScan(basePackages = "com.edutest.persistance.entity")
+@EnableJpaAuditing
 public class EdutestWebServerApplication {
 
     public static void main(String[] args) {
