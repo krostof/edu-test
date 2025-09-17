13576544507eb670286d6847590ef802ec684d52
krostof <salacink@wp.pl>
2025-09-17 21:32:52 +0200

    Used @Getter

--- edutest-domain/src/main/java/com/edutest/domain/assignment/ValidationResult.java
+++ edutest-domain/src/main/java/com/edutest/domain/assignment/ValidationResult.java
@@ -24,10 +24,6 @@ public class ValidationResult {
                 .build();
     }
 
-    public boolean isValid() {
-        return valid;
-    }
-
     public boolean hasError() {
         return !valid;
     }
